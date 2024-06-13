add85e649f0741570a5dd9dff3a532940d98b757
EnricoClem <enrico.clem94@gmail.com>
2024-06-13 17:49:46 +0200

    ADD: basic style

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -3,10 +3,17 @@ import axios from 'axios'
 export default {
   data() {
     return {
-      projects: []
+      projects: [],
+      currentPage: 1,
+      lastPage: null
     }
   },
   methods: {
+    changePage(n) {
+      if(n === this.currentPage) return
+      this.currentPage = n
+      this.fetchProjects()
+    },
     fetchProjects() {
       axios.get('http://127.0.0.1:8000/api/projects',{
         params: {
@@ -32,24 +39,26 @@ export default {
   <main>
     <section class="h-100">
       <div class="container py-5 h-100">
-        <div class="row aling-items-center flex-column h-100">
+        <div class="h-100">
           <div class="col-auto text-center">
             <h1>My projects:</h1>
           </div>
-          <div class="col-auto text-center">
-            <ul>
-              <li v-for="project in projects" key="project.id">
-                <h3>{{ project.title }}</h3>
-                <p>{{ project.description }}</p>
-                <a :href="`{{ project.project_link }}`"></a>
+          <div class="">
+            <ul class="row row-cols-1 row-cols-md-2 g-4">
+              <li class="col" v-for="project in projects" key="project.id">
+                <div class="card">
+                  <h3 class="card-header">{{ project.title }}</h3>
+                  <div class="card-body card-text">
+                    <p>{{ project.description }}</p>
+                    <a :href="`${project.project_link}`">{{ project.project_link }}</a>
+                  </div>
+                </div>
               </li>
             </ul>
           </div>
           <div>
-            <ul>
-              <li v-for="n in lastPage" :key="n">
-                {{ n }}
-              </li>
+            <ul class="text-center">
+              <button class="btn btn-primary m-3 col-2" @click="changePage(n)" v-for="n in lastPage" :key="n">{{ n }}</button>
             </ul>
           </div>
         </div>
